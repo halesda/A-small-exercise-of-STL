df35e5c817e6480fe098d96721247099407abe10
halesda <1836250117@qq.com>
2023-04-24 09:58:25 +0800

    测试vector

--- test/main1.cpp
+++ test/main1.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 using namespace std;
-#include"../allocator.h"
+#include"../vector.h"
 
 int main()
 {
+	my_stl::vector<int> v;
+	for(int i = 0;i < 10;++i)
+	{
+		v.push_back(i);
+	}
+
 	return 0;
 }
